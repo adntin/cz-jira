f31f0a94746ee4009b7e2d6344261fcc73f7cac0
林斌 <linbin@LDS-Z-C7325.local>
2019-05-22 11:33:02 +0800

    添加comment测试

--- src/test.js
+++ src/test.js
@@ -10,11 +10,25 @@ const jira = new JiraConnector({
   },
 });
 
-jira.issue.getIssue({ issueKey: 'IPC-34' }, (error, issue) => {
-  if (error) {
-    console.log('fetch jira data error: ', error);
+// 获取issue
+// https://github.com/floralvikings/jira-connector/blob/master/api/issue.js
+// https://developer.atlassian.com/cloud/jira/platform/rest/v3/#api-rest-api-3-issue-issueIdOrKey-get
+// jira.issue.getIssue({ issueKey: 'IPC-34' }, (error, issue) => {
+//   if (error) {
+//     console.log('fetch jira data error: ', error);
+//   } else {
+//     console.log('fetch jira data success: ', issue);
+//   }
+// });
+
+// 添加comment
+// https://github.com/floralvikings/jira-connector/blob/master/api/issue.js
+// https://developer.atlassian.com/cloud/jira/platform/rest/v3/#api-rest-api-3-issue-issueIdOrKey-comment-post
+jira.issue.addComment({ issueKey: 'SOFT-2244', comment: { body: new Date()}}, (error, comment) => {
+    if (error) {
+    console.log('set jira comment error: ', error);
   } else {
-    console.log('fetch jira data success: ', issue);
+    console.log('set jira comment success: ', comment);
   }
 });
 
